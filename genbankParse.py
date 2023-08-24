5d41f398d3cc390dd7d772467eb2dcc8a7094f8a
Tyler Gaw <tagaw@ucsc.edu>
2023-08-24 11:16:12 -0700

    dirty code, fixed edge cases

--- genbankParse.py
+++ genbankParse.py
@@ -3,7 +3,7 @@ from Bio.SeqRecord import SeqRecord
 from Bio.SeqFeature import SimpleLocation
 from typing import Iterator
 
-tempfile = 'tests/pSPDY.gb'
+tempfile = 'tests/edge_case.gb'
 
 def Stealth_CDS_Extract(gbfile: str) -> Iterator:
     """
@@ -35,26 +35,100 @@ def parsePlasmid(gbfile: str) -> tuple[list[SimpleLocation]]:
     gb = SeqIO.parse(gbfile,'genbank') 
     for rec in gb:
         for feature in rec.features:
-            if not (feature.type == 'CDS' or feature.type == 'ORF'):
+            if feature.type in {'source','gene'}:
+                continue
+            if not (feature.type in {'CDS','ORF'}):
                 avoid.append((feature.location, feature.location.start % 3))
             else:
                 cds.append((feature.location, feature.location.start % 3))
     return avoid,cds
 
 one,two = parsePlasmid(tempfile)
+def _trimStart(loc_list: list[SimpleLocation], cds: SimpleLocation)  -> list[SimpleLocation]:
+    ref = SimpleLocation(cds.start,cds.start+3,cds.strand)
+    for i in range(len(loc_list)):
+        loc = loc_list[i]
+        if loc.start < ref.start and loc.end > ref.end:
+            loc_list[i] = SimpleLocation(loc.start,ref.start,loc.strand)
+            loc_list.insert(i+1,SimpleLocation(ref.end,loc.end,loc.strand)) 
+            break  
+        if loc.start in ref:
+            loc_list[i]= SimpleLocation(ref.end,loc.end,loc.strand)
+            break
+        elif loc.end in ref:
+            loc_list[i] = SimpleLocation(loc.start,ref.start,loc.strand)
+            break
+    return
 
-def defineMutable(location: tuple[list[SimpleLocation]]) -> list[SimpleLocation]:
+def _frameAdj(loc_list: list[SimpleLocation], frame: int) -> SimpleLocation:
+    'helper func'
+    new = []
+    for loc in loc_list:
+        st = loc.start
+        ed = loc.end
+        for _ in range(1,3):
+            if st % 3 != frame :
+                st += 1
+            if ed % 3 != frame:
+                ed -= 1
+        if (ed-st) < 3:
+            continue
+        adj = SimpleLocation(st,ed,loc.strand)
+        new.append(adj)
+    return new
+
+def _removeOverlap(loc_list: list[SimpleLocation],cds: SimpleLocation) -> list[SimpleLocation]:
+    ret = []
+    for loc in loc_list:
+        new = [[loc.start,loc.end]]
+        if loc.start in cds and (loc.end in cds or loc.end == cds.end):
+            'completely removes region'
+            continue
+        if loc.start < cds.start and loc.end > cds.end:
+            'splits range into two'
+            dummy = [cds.end,loc.end]
+            new[-1][1] = cds.start
+            new.append(dummy) 
+        elif loc.start in cds:
+            'truncates start'
+            new[-1][0] = cds.end
+        elif loc.end in cds or loc.end == cds.end:
+            'truncates end'
+            new[-1][1] = cds.start
+        ret.extend([SimpleLocation(i,j,loc.strand) for i,j in new])
+    return ret
+
+def _windowCorrection(old: list[list[SimpleLocation],int,SimpleLocation], st_bound: int, ed_bound: int) -> list[list[SimpleLocation],int,SimpleLocation]:
+    loc,fm,parent = old
+    new = SimpleLocation(st_bound,ed_bound,parent.strand)
+    dummy = []
+    for i in loc:
+        if st_bound > i.end:
+            continue
+        if i.start in new and (i.end in new or i.end == new.end):
+            dummy.append(i)
+        elif i.start in new:
+            dummy.append(SimpleLocation(i.start,ed_bound,i.strand))     
+            continue
+        elif i.end in new or i.end == new.end:
+            dummy.append(SimpleLocation(st_bound,i.end,i.strand))
+            continue 
+    return [dummy,fm,new]
+
+def _defineMutable(location: tuple[list[SimpleLocation]]) -> list[SimpleLocation]:
     avoid,cdsRegions = location
     temp = [] #[[[SimpLocation List],frame,parent CDS]]
 
     'Removes mutable cds regions from overlapping non-coding sequence'
     for cds,frame in cdsRegions:
+        if len(cds) <= 3:
+            continue
         bounds = [[cds.start,cds.end]]
         for loc,_ in avoid:
             if cds.start > loc.start and cds.end < loc.end:
                 'case: interior ORF to non-coding region'
                 break
-            if bounds[-1][0] < loc.start and bounds[-1][1] > loc.end:
+            if bounds[-1][0] < loc.start and bounds[-1][1] >loc.end:
                 'case: non-coding region interior to ORF'
                 new_bound = [loc.end,cds.end]
                 bounds[-1][1] = loc.start
@@ -62,35 +136,20 @@ def defineMutable(location: tuple[list[SimpleLocation]]) -> list[SimpleLocation]
             elif bounds[-1][0] in loc:
                 'case: clipped start'
                 bounds[-1][0] = loc.end
-            elif bounds[-1][1] in loc:
+            elif bounds[-1][1] in loc or bounds[-1][1] == loc.end:
                 'case: clipped end'
                 bounds[-1][1] = loc.start
-        if bounds[-1][1] <= bounds[-1][0]:
+        if (bounds[-1][1] - bounds[-1][0]) <= 3:
             del bounds[-1]
-        if not bounds:
+        if len(bounds) <= 0:
             continue
         mut_range = [SimpleLocation(start=rng[0],end=rng[1],strand=cds.strand) for rng in bounds ]
         temp.append([mut_range,frame,cds])
 
     pp(temp)
 
-    def _trimStart(loc_list: list[SimpleLocation], cds: SimpleLocation)  -> list[SimpleLocation]:
-            ref = SimpleLocation(cds.start,cds.start+3,cds.strand)
-            for i in range(len(loc_list)):
-                loc = loc_list[i]
-                if loc.start < ref.start and loc.end > ref.end:
-                    loc_list[i] = SimpleLocation(loc.start,ref.start,loc.strand)
-                    loc_list.insert(i+1,SimpleLocation(ref.end,loc.end,loc.strand)) 
-                    break  
-                if loc.start in ref:
-                    loc_list[i]= SimpleLocation(ref.end,loc.end,loc.strand)
-                    break
-                elif loc.end in ref:
-                    loc_list[i] = SimpleLocation(loc.start,ref.start,loc.strand)
-                    break
-            return
 
-            
+    'Trimming starts'   
     for i in range(len(temp)):
         tLoc = temp[i][0]
         frm = temp[i][1]
@@ -107,68 +166,14 @@ def defineMutable(location: tuple[list[SimpleLocation]]) -> list[SimpleLocation]
                 
 
     'frame adjusts mutable fragments to lie in frame of parent CDS'
-    def _frameAdj(loc_list: list[SimpleLocation], frame: int) -> SimpleLocation:
-        'helper func'
-        new = []
-        for loc in loc_list:
-            st = loc.start
-            ed = loc.end
-            for _ in range(1,3):
-                if st % 3 != frame :
-                    st += 1
-                if ed % 3 != frame:
-                    ed -= 1
-            if (ed-st) < 3:
-                continue
-            adj = SimpleLocation(st,ed,loc.strand)
-            new.append(adj)
-        return new
-    
+  
     for i in range(len(temp)):
         temp[i][0] = _frameAdj(temp[i][0],temp[i][1])
 
     
     temp = sorted(temp,key = lambda x: len(x[2]), reverse=True)
 
-    def _removeOverlap(loc_list: list[SimpleLocation],cds: SimpleLocation) -> list[SimpleLocation]:
-        ret = []
-        for loc in loc_list:
-            new = [[loc.start,loc.end]]
-            if loc.start in cds and loc.end in cds:
-                'completely removes region'
-                continue
-            if loc.start < cds.start and loc.end > cds.end:
-                'splits range into two'
-                temp = [cds.end,loc.end]
-                new[-1][1] = cds.start
-                new.append(temp) 
-            elif loc.start in cds:
-                'truncates start'
-                new[-1][0] = cds.end
-            elif loc.end in cds:
-                'truncates end'
-                new[-1][1] = cds.start
-            ret.extend([SimpleLocation(i,j,loc.strand) for i,j in new])
-        return ret
-    
-    def _windowCorrection(old: list[list[SimpleLocation],int,SimpleLocation], st_bound: int, ed_bound: int) -> list[list[SimpleLocation],int,SimpleLocation]:
-        loc,fm,parent = old
-        new = SimpleLocation(st_bound,ed_bound,parent.strand)
-        temp = []
-        for i in loc:
-            if st_bound > i.end:
-                continue
-            if i.start in new and i.end in new:
-                temp.append(i)
-            elif i.start in new:
-                temp.append(SimpleLocation(i.start,ed_bound,i.strand))     
-                continue
-            elif i.end in new:
-                temp.append(SimpleLocation(st_bound,i.end,i.strand))
-                continue
-            
-        return [temp,fm,new]
-
+    'remove overlaps'
     for i in range(len(temp)):
         frag = temp[i][0]
         frm = temp[i][1]
@@ -183,8 +188,7 @@ def defineMutable(location: tuple[list[SimpleLocation]]) -> list[SimpleLocation]
             if parent_cds.start in region or parent_cds.end in region:
                 if parent_frame != frm:
                    temp[i][0] = _removeOverlap(frag,parent_cds)
-                if parent_frame == frm:
-                    pass
+                   frag = temp[i][0]
             if parent_cds.start in region and parent_cds.end in region:
                 temp[i+j+1][2] = None
             elif parent_cds.start in region:
